2fe89fa8bb0a150c1327fac5e6af9a9596252dd0
v_hcjhe(何初炬) <v_hcjhe@tencent.com>
2018-10-14 20:35:52 +0800

    Java文件

---
+++ Demo1.java
@@ -0,0 +1,23 @@
+package cn.tx;
+
+public class Demo1 {
+	
+   public static void main(String[] args) {
+	
+	   int java = 82;
+	   
+	   if(java > 80) {
+		   System.out.println("优秀成绩");
+	   }
+	   else if(java > 70) {
+		   System.out.println("成绩一般");
+	   }
+	   else if(java > 60) {
+		   System.out.println("及格");
+	   }
+	   else {
+		   System.out.println("不及格");
+	   }
+   }	
+   
+}
